8b4b23111271984fdbc5b513b5845fe3abf81229
Ishoboy <francislandia@icloud.com>
2022-09-27 22:53:11 +0800

    added welcome message and login form components

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -6,13 +6,28 @@ const Layout = props => {
         <>
             <Navbar/>
 
+                <div className="container py-4">
+
+                    <div className="row">
+
+                        <div className="front-card col-xs-10 col-xs-offset-1">
+
+                        <Welcome />
+                        <Login/>
+                            {props.children}
+
+                        </div>
+
+                    </div>
+
+                </div>
         </>
     )
 }
 
 const Navbar = props => {
     return(
-        <nav className="navbar navbar-default navbar-fixed-top">
+        <nav className="navbar navbar-light bg-dark navbar-fixed-top">
             <div className="container">
                 <div className="navbar-header">
                     <a className="navbar-brand" href="#">
@@ -35,5 +50,45 @@ const Navbar = props => {
         </nav>
     )
 }
+
+const Login = props => {
+    return(
+        <div className="log-in col-xs-4 col-xs-offset-1">
+            <form>
+                <div className="form-group">
+                    <input type="text" className="form-control username" placeholder="Username"/>
+                </div>
+                <div className="form-group">
+                    <input type="text" class="form-control password" placeholder="Password"/>
+                </div>
+                <br/>
+                <button id="log-in-btn" className="btn btn-default btn-primary col-xs-3 col-xs-3 col-xs-offset-1">Log in</button>
+                <br></br>
+                <label>
+                    <input type="checkbox"/>
+                    <span>Remember me</span>
+                    <span> &#183; </span>
+                </label>
+                    <a href="#">Forgot password?</a>
+            </form>
+        </div>
+    )
+}
+
+const Welcome = props => {
+    return (
+        <div className="col-xs-6 welcome">
+            <div id="welcome-text">
+            <h1><strong>Welcome to Twitter.</strong></h1>
+            <p>Connect with your friends &#8212; and other fascinating people. Get in-the-moment updates on the things that interest you. And watch events unfold, in real time, from every angle.</p>
+            </div>
+            <p><a href="https://confident-murdock-8e5bba.netlify.app/" target="_blank" rel="noopener noreferrer" id="twit-info">Francis Artemio Landia</a></p>
+            <p>Full Stack Twitter Clone project at<a href="https://www.altcademy.com/" target="_blank" rel="noopener noreferrer"> Altcademy</a></p>
+        </div>
+    )
+}
+
+
+
 
 export default Layout
