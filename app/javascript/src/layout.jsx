31a39f63ddd2e2065c658cf1a77da67b0822e627
Ishoboy <francislandia@icloud.com>
2022-09-30 22:10:11 +0800

    added footer and added additional div for homeback id change the background image installed fontawesome for social media icons

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faLinkedin, faInstagram, faYoutube, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 import "./layout.scss";
 
 const Layout = props => {
@@ -8,23 +9,26 @@ const Layout = props => {
         <>
             <Navbar/>
 
-                <div className="main">
+                <div id="homeback">
+                    <div className="main">
 
-                    <div className="container">
+                        <div className="container">
 
-                        <div className="row">
+                            <div className="row">
 
-                            <div className="front-card col-xs-10 col-xs-offset-1">
-                                <Welcome />
-                                <Login/>
-                                <Signup/>
-                   
-                                    {props.children}
+                                <div className="front-card col-xs-10 col-xs-offset-1">
+                                    <Welcome />
+                                    <Login/>
+                                    <Signup/>
+                                    <Footer/>
+
+                                        {props.children}
+                                </div>
                             </div>
+
                         </div>
 
                     </div>
-
                 </div>
         </>
     )
@@ -115,13 +119,68 @@ const Welcome = props => {
         <div className="col-xs-6 welcome">
             <div id="welcome-text">
             <h1><strong>Welcome to Twitter.</strong></h1>
-            <p>Connect with your friends &#8212; and other fascinating people. Get in-the-moment updates on the things that interest you. And watch events unfold, in real time, from every angle.</p>
+            <p>Connect with your friends &#8212; and other fascinating people.</p>
+
+            <p>Get in-the-moment updates on the things that interest you. And watch events unfold, in real time, from every angle.</p>
             </div>
-            <p><a href="https://confident-murdock-8e5bba.netlify.app/" target="_blank" rel="noopener noreferrer" id="twit-info">Francis Artemio Landia</a></p>
-            <p>Full Stack Twitter Clone project at<a href="https://www.altcademy.com/" target="_blank" rel="noopener noreferrer"> Altcademy</a></p>
         </div>
     )
 }
+
+const Footer = () => {
+    return (
+      <div id="footer">
+              <div className="text-center ms-4 pt-3">
+                    © <a href="https://confident-murdock-8e5bba.netlify.app/"
+                        target="_blank"rel="noopener noreferrer">
+                        <em>Francis Artemio Landia </em>
+                    </a>
+
+                    <p>2022</p>
+
+                    <div className="d-flex justify-content-center">
+                        <a href="https://github.com/Franz-Art-L"
+                        className="btn btn-danger"
+                        id="github"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faGithub} />
+                            
+                        </a>
+
+                        <a
+                        href="https://www.linkedin.com/in/francis-artemio-landia-9a1375123/"
+                        className="btn btn-warning"
+                        id="Linkedin"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faLinkedin} />
+                        </a>
+
+                        <a
+                        href="https://www.instagram.com/landiafrancisartemio/"
+                        className="btn btn-success"
+                        id="Linkedin"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faInstagram} />
+                        </a>
+                        
+                    </div>
+                   
+                   
+              </div>
+           
+
+          <div className="text-center">
+               <p>Full Stack Twitter Clone project at<a href="https://www.altcademy.com/" target="_blank" rel="noopener noreferrer"> Altcademy</a></p>
+         </div>
+
+          
+    </div>     
+    );
+};
+  
 
 
 export default Layout
